7b3142f4d37ba2d4b792ac38fdcef25f50b78459
NickTJLee <NickTJLee@altek.com.tw>
2022-03-05 13:35:41 +0800

    update linkedlist sample.

---
+++ linkedlist.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+struct ListNode
+{
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {};
+	ListNode(int x) : val(x),next(nullptr) {};
+	ListNode(int x, ListNode *tpval) : val(x), next(tpval){}
+};
+
+void travelList(ListNode *ptHead)
+{
+	ListNode *node = ptHead;
+	while(node!= nullptr)
+	{
+		printf("%d ", node->val);
+		node = node->next;
+	}
+	return;
+}
+
+void append(ListNode **ppthead, int dVal)
+{
+	ListNode *cur = *ppthead;
+	ListNode *pre = nullptr;
+	while(cur != nullptr)
+	{
+		pre = cur;
+		cur = cur->next;
+	}
+	ListNode *NewNode = new ListNode(dVal);
+	
+	if(pre)
+		pre->next = NewNode;
+	else
+		*ppthead = NewNode;
+		
+	return;
+}
+
+void appendIndirect(ListNode **pptHead, int dVal)
+{
+	ListNode **indirect = pptHead;
+	while(*indirect)
+	{
+		indirect = &(*indirect)->next;
+	}
+	*indirect = new ListNode(dVal);
+}
+
+void removeList(ListNode **pptHead, ListNode *target)
+{
+	ListNode **indirect = pptHead;
+	while(*indirect != target)
+	{
+		indirect = &(*indirect)->next;
+	}
+	(*indirect) = target->next;
+	delete target;
+}
+
+int main()
+{
+    ListNode *pHead = nullptr;
+    for(int i=0;i<10;i++)
+    	appendIndirect(&pHead, i);
+    travelList(pHead);
+    printf("\n");
+    
+    removeList(&pHead, pHead->next->next);
+    removeList(&pHead, pHead->next->next->next->next->next->next);
+    
+    travelList(pHead);
+    
+    printf("\n");
+    system("pause");
+    return 0;
+}
+
